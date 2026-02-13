145c39d029385918b692d460554e55968c1e3e8f
Alfredo García Suárez <110580458+freddy-7@users.noreply.github.com>
2026-02-13 08:54:33 -0600

    Add files via upload

---
+++ Matrices_1.m
@@ -0,0 +1,14 @@
+%Creación de una matriz A
+A = [1 1 1; 3 5 4; 3 6 5]
+
+%Obtener el determinante de una matriz A
+Determinante= det(A)
+
+%Obtener la matriz transpuesta de una matriz A
+M_transpuesta= transpose(A)
+
+%Obtener la matriz inversa de una matriz A
+M_inversa= inv(A)
+
+%Multiplicación de la matriz A y su matriz inversa 
+Identidad= A*inv(A)
